74046bcf20d7d6e75e0f38f53bd24aef167b83f0
huzaifa-1106 <huzaifa@esgtree.com>
2025-03-31 14:54:31 -0400

    Backend FrameWorks

--- src/main/java/Backend/Faculty.java
+++ src/main/java/Backend/Faculty.java
@@ -56,4 +56,90 @@ public class Faculty {
         return String.join(", ", coursesOffered);
     }
 
-    p
+    public String getEmail() {
+        return email;
+    }
+    public static Faculty findByName(String name) {
+        for (Faculty f : facultyList) {
+            if (f.getName().equalsIgnoreCase(name.trim())) {
+                return f;
+            }
+        }
+        return null;
+    }
+    public String getOfficeLocation() {
+        return officeLocation;
+    }
+
+    // Setters
+    public void setFacultyID(String facultyID) {
+        this.facultyID = facultyID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfilePhoto(Image profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public void setResearchInterest(String researchInterest) {
+        this.researchInterest = researchInterest;
+    }
+
+    public void setCoursesOffered(List<String> coursesOffered) {
+        this.coursesOffered = coursesOffered;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setOfficeLocation(String officeLocation) {
+        this.officeLocation = officeLocation;
+    }
+
+
+
+    // Getters
+    public String getFacultyID() {
+        return facultyID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Image getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public List<String> getCoursesOffered() {
+        return coursesOffered;}
+
+
+    public String getResearchInterest() {
+        return researchInterest;
+    }
+
+    // Display method
+    public void displayFacultyInfo() {
+        System.out.println("Faculty ID: " + facultyID);
+        System.out.println("Faculty Name: " + name);
+        System.out.println("Degree: " + degree);
+        System.out.println("Research Interest: " + researchInterest);
+        System.out.println("Courses Offered: " + coursesOffered);
+        System.out.println("Email: " + email);
+        System.out.println("Office Location: " + officeLocation);
+    }
+}
+
